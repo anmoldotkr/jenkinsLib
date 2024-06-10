f501e87c01b22351502e66fb2557314393420f63
anmolkumar09 <120151136+anmolkumar09@users.noreply.github.com>
2024-06-10 18:32:16 +0530

    Update abc.groovy

--- vars/abc.groovy
+++ vars/abc.groovy
@@ -3,10 +3,10 @@ def call(){
 }
 
 void sub(int a,int b){
-  def a=90
-  def b=90
-  def res = a+b
+  def a1=90
+  def b1=90
+  def res1 = a1+b1
   
-  println "${a}+${b} = ${res}"
+  println "${a1}+${b1} = ${res1}"
   
 }
